6c6c59fd004f4546499fd39ddfde8e5cb5d8eed6
MaryAnzh <MaryAnzh@mail.ru>
2022-08-25 21:52:26 +0400

    feat: add cv, core and share modules

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # App-Cv
 
 **deploy:**
-
+https://maryanzh.github.io/app-cv/
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'cv', loadChildren: () => import('./cv/cv.module').then(m => m.CvModule) }, { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
